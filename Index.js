b8d66374346aa508b4777d4686ea97f2cdf5a8dc
Muhammad Zubair <2020cs601@student.uet.edu.pk>
2022-08-19 15:27:39 +0500

    Events in Node JS

--- Index.js
+++ Index.js
@@ -1,7 +1,18 @@
-const os=require("os")
+const EventEmitter=require('events')
+let express=require('express')
+let App=express()
+let Event=new EventEmitter()
 
-console.log(os.freemem()/(1024*1024*1024))
-console.log(os.hostname())
-console.log(os.platform())
-console.log(os.arch())
-console.log(os.userInfo().username)
+let Count=0
+
+Event.on("Counter" , ()=>
+{
+    ++Count;
+    console.log(Count)
+})
+App.get('/' , (Req,Res)=>{
+    Event.emit("Counter")
+    Res.send("Event Emitted")
+})
+
+App.listen(3000)
