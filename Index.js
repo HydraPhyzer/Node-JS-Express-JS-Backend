cec4465517714a8789af907eb10a687b0c1b2af9
Muhammad Zubair <2020cs601@student.uet.edu.pk>
2022-08-19 13:00:38 +0500

    Upload Files With Multer

--- Index.js
+++ Index.js
@@ -1,63 +1,24 @@
+const multer=require('multer')
 let express = require("express");
-const mongoose = require("mongoose");
-require("./Mongo");
-let Model = require("./Schema");
 let App = express();
+// App.use(express.json());
 
-App.use(express.json())
 
-App.post('/post', async (Req, Res) => {
-  let Data = new Model(Req.body);
-  let Result=await Data.save()
-  Res.send(Result);
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, 'Uploads')
+    },
+    filename: function (req, file, cb) {
+      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9) + ".jpg"
+      cb(null, file.fieldname + '-' + uniqueSuffix)
+    }
+  })
+  
+  const upload = multer({ storage: storage }).single('Filer')
+
+App.post("/upload", upload ,async (Req, Res) => {
+    Res.send("File Uploaded")
 });
-App.get('/get' , async(Req,Res)=>
-{
-    let Result=await Model.find();
-    Res.send(Result)
-})
 
-App.put('/put/:_id' , async (Req,Res)=>
-{
-    let Data=await Model.updateOne(
-        Req.params , 
-        {
-            $set:Req.body
-        }
-    )
-    Res.send(Data)
-})
-
-App.delete('/delete/:_id' , async (Req,Res)=>
-{
-    let Result=await Model.deleteOne(Req.params)
-    Res.send(Result)
-})
-
-App.get('/search/name/:key' , async (Req,Res)=>
-{   
-    let Data=await Model.find(
-        {
-            "$or":
-            [
-                {"Name":{$regex:Req.params.key}},
-            ]
-        }
-    )
-    Res.send(Data)
-})
-App.get('/search' , async (Req,Res)=>
-{   
-    let Data=await Model.find(
-        {
-            "$or":
-            [
-                // {"Age":{$regex:Req.params.key}},
-            ]
-        }
-    )
-    // console.log(Req.query)
-    // Res.send(Data)
-})
-
-App.listen(3000)
+App.listen(3000);
